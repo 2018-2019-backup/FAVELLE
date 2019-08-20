ce54741b5193efdcfd505acdb47ec911aa35c00a
KKMSoft-SourceCode <52309871+KKMSoft-SourceCode@users.noreply.github.com>
2019-08-20 11:37:49 +0530

    Add files via upload

---
+++ FavelleVaultws/WsInterface.vb
@@ -0,0 +1,46 @@
+﻿Imports Autodesk.Connectivity.WebServices
+Imports VDF = Autodesk.DataManagement.Client.Framework
+Imports Autodesk.DataManagement.Client.Framework.Vault.Currency.Connections
+Imports System.IO
+Imports System.Data
+Imports System.Xml
+Imports Autodesk.Connectivity.Extensibility.Framework
+
+<Assembly: Autodesk.Connectivity.Extensibility.Framework.ExtensionId("9a2be05c-54b1-416a-801d-9f5459b7a2ef")> 
+
+' This number gets incremented for each Vault release.
+<Assembly: Autodesk.Connectivity.Extensibility.Framework.ApiVersion("9.0")> 
+
+Public Class WsInterface
+    Implements IWebServiceExtension
+    Dim vConnection As Connection
+
+    Public Sub OnLoad() Implements IWebServiceExtension.OnLoad
+        AddHandler ItemService.UpdateItemLifecycleStateEvents.GetRestrictions, AddressOf UpdateItemLifecycleStateEvents_GetRestrictions
+    End Sub
+
+    Private Sub UpdateItemLifecycleStateEvents_GetRestrictions(sender As Object, e As Object)
+        Dim itemMasterId As Long = DirectCast(e, Autodesk.Connectivity.WebServices.UpdateItemLifeCycleStateCommandEventArgs).ItemMasterIds(0)
+
+        Dim CurrentCredentials As New Autodesk.Connectivity.WebServicesTools.WebServiceCredentials(DirectCast(sender, IWebService))
+        Dim ServiceManager As New Autodesk.Connectivity.WebServicesTools.WebServiceManager(CurrentCredentials)
+
+        Dim userId = CurrentCredentials.SecurityHeader.UserId
+        vConnection = New Connection(ServiceManager, vaultName, userId, vaultServer, VDF.Vault.Currency.Connections.AuthenticationFlags.Standard)
+
+        Dim _item As Autodesk.Connectivity.WebServices.Item = 
+        Dim itemId As Long = _item.Id
+        Dim destState As String = String.Empty
+        Dim curLyfCyc() As Autodesk.Connectivity.WebServices.LfCycDef = ServiceManager.DocumentServiceExtensions.GetLifeCycleDefinitionsByIds(New Long() {_file.FileLfCyc.LfCycDefId})
+        If Not curLyfCyc(0).DispName.Equals(LifecycleName, StringComparison.InvariantCultureIgnoreCase) Then
+            Exit Sub
+        End If
+        For Each _state As LfCycState In curLyfCyc(0).StateArray
+            If _state.Id.Equals(DirectCast(e, UpdateFileLifeCycleStateCommandEventArgs).ToStateIds(0)) Then
+                destState = _state.DispName
+                Exit For
+            End If
+        Next
+    End Sub
+
+End Class
---
+++ FavelleVaultws/uiSelectAX.xaml.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Data
+
+Public Class uiSelectAX
+    Public dtAxOrgs As DataTable
+    Public Sub New(ByVal lstOrgs As List(Of String))
+        InitializeComponent()
+        dtAxOrgs = New DataTable
+        dtAxOrgs.Columns.Add("IsSelected", System.Type.GetType("System.Boolean"))
+        dtAxOrgs.Columns.Add("OrgName", System.Type.GetType("System.String"))
+        Dim dr As DataRow
+        For Each _org As String In lstOrgs
+            dr = dtAxOrgs.NewRow()
+            dr(0) = False
+            dr(1) = _org
+            dtAxOrgs.Rows.Add(dr)
+        Next
+        lstAxOrgs.ItemsSource = dtAxOrgs.DefaultView()
+    End Sub
+
+    Private Sub btnImport_Click_1(sender As Object, e As RoutedEventArgs)
+        Try
+            If dtAxOrgs.Select("IsSelected = 'True'").Count() > 0 Then
+                Me.DialogResult = True
+            Else
+                MessageBox.Show("Kindly select atleast one organisation", msgCaption, MessageBoxButton.OK, MessageBoxImage.Information)
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
