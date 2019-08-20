5777f4322889acb9c4586142947d1b76ee067691
KKMSoft-SourceCode <52309871+KKMSoft-SourceCode@users.noreply.github.com>
2019-08-20 11:34:14 +0530

    Add files via upload

---
+++ FavelleVaultJobs/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("FavelleVaultJobs")> 
+<Assembly: AssemblyCopyright("Copyright © KKM 2016")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e203aad7-6eb8-43d1-9e21-2f4558caa829")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.3.0")> 
+<Assembly: AssemblyFileVersion("1.0.3.0")> 
+
+<Assembly: AssemblyDescription("Custom Job Processor for Favelle")> 
+<Assembly: AssemblyCompany("KKM SOFT PVT LTD")>
